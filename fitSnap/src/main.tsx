ba425b5d8addecf395bd329be4c2bff85a02b1f5
MichellePtrciaa <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-12-14 13:01:54 +0700

    Update main.tsx

--- fitSnap/src/main.tsx
+++ fitSnap/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import router from './routes/router'
 import { RouterProvider } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
- 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
